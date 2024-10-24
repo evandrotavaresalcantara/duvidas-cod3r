11c0b55a7d5bc5c4c162b42633f85713f3e21254
evandrotavaresalcantara <evandrotavaresalcantara@gmail.com>
2024-10-24 10:46:16 -0300

    inicial

--- src/app/(externa)/extranet/page.tsx
+++ src/app/(externa)/extranet/page.tsx
@@ -4,14 +4,11 @@ import UsuarioContext from "@/app/data/context/UsuarioContext"
 import { useContext } from "react"
 
 export default function PaginaExtranet() {
-    const { usuario, atualizarUsuario } = useContext(UsuarioContext)
+    const { usuario } = useContext(UsuarioContext)
     return (
         <Pagina externa>
             Página Extranet
             <span className="text-red-500 border border-red-500 m-4">{usuario?.nome}</span>
-            <button onClick={atualizarUsuario}>
-                Atualizar Nome
-            </button>
         </Pagina>
     )
 }
